ba3af28ed259b4b80481f36c39ea937e63e2107c
Kyri Kontozis <kyriacos.kontozis@energyexemplar.com>
2019-04-10 15:19:59 +0930

    Split the resource datatemplates to discrete files to make more managable

---
+++ UI.WPF/ResourceDictionaries/DataTemplates/DataSourceDataTemplates.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UI.WPF.ResourceDictionaries
+{
+    public partial class DataSourceDataTemplates : ResourceDictionary
+    {
+        public DataSourceDataTemplates()
+        {
+            InitializeComponent();
+        }
+    }
+}
