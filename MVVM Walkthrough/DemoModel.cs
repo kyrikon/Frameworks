4db75e19ec5fead52f5b0f5884a49c3f5d312942
Kyri Kontozis <kyrikon@gmail.com>
2022-10-16 20:54:24 +1030

    Demo for i

---
+++ MVVM Walkthrough/DemoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Walkthrough
+{
+    interface IDemoModel
+    {
+        string StringValue { get; set; }
+        int IntValue { get; set; }
+        bool BoolValue { get; set; }
+    }
+    public class DemoModel : IDemoModel
+    {
+        public string StringValue { get; set; } = "A string";
+        public int IntValue { get; set; } = 10;
+        public bool BoolValue { get; set; } = false;
+    }
+}
---
+++ MVVM Walkthrough/MainWindowVM.cs
@@ -0,0 +1,65 @@
+﻿using Core.Helpers;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Walkthrough
+{
+    sealed internal class MainWindowVM : NotifyPropertyChanged
+    {
+        public string SomeText
+        {
+            get
+            {
+                return GetPropertyValue<string>();
+            }
+            set
+            {
+                SetPropertyValue<string>(value);
+            }
+        }
+        public int CountClicks
+        {
+            get
+            {
+                return GetPropertyValue<int>();
+            }
+            set
+            {
+                SetPropertyValue<int>(value);
+            }
+        }
+        public bool IsTBEnabled
+        {
+            get
+            {
+                return GetPropertyValue<bool>();
+            }
+            set
+            {
+                SetPropertyValue<bool>(value);
+            }
+        }
+        public DelegateCommand ClickCmd
+        {
+            get;
+            private set;
+        }
+        public MainWindowVM(IDemoModel Model)
+        {
+            SomeText = Model.StringValue;
+            CountClicks = Model.IntValue;
+            IsTBEnabled = Model.BoolValue;
+            ClickCmd = new DelegateCommand(delegate (object o)
+            {
+                CountClicks++;
+                IsTBEnabled = !IsTBEnabled;
+
+            });
+        }
+       
+    }
+}
